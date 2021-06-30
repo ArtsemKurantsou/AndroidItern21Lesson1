766faefd2b08c466e938939bab45a94d41171a57
Artem Kurantsov <akurantsov@arlo.com>
2021-06-30 23:32:36 +0300

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+    solution()
+}
+
+fun getBill(): Array<Any> {
+    return arrayOf(
+        "cashier#1",
+        "100$",
+        3,
+        "water",
+        "espresso",
+        "caesar"
+    )
+}
+
+fun solution() {
+
+}
